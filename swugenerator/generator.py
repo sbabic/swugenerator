490f0ad7d5adb86b3616bfcccbbad10ca102b275
yh-sb <yh.sb@outlook.com>
2024-03-17 12:29:18 +0200

    Add support symlinks in zstd
    Error before this fix:
    `Warning : /home/test/dev/proj1/buildroot/output/images/rootfs.ext4 is a symbolic link, ignoring`

--- swugenerator/generator.py
+++ swugenerator/generator.py
@@ -108,6 +108,7 @@ class SWUGenerator:
                         "-z",
                         "-k",
                         "-T0",
+                        "-f",
                         "-c",
                         new.fullfilename,
                         ">",
