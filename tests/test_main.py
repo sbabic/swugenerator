ef6e2e7e54cb32f0f9e2125068a502b545ac411f
Javier Fernandez Pastrana <javier.pastrana@linutronix.de>
2025-05-23 13:08:44 +0200

    Add test cases for CMS keyform option
    Add test cases covering various uses of the keyform argument, with and
    without additional options.
    
    Signed-off-by: Javier Fernandez Pastrana <javier.pastrana@linutronix.de>

--- tests/test_main.py
+++ tests/test_main.py
@@ -154,7 +154,7 @@ def test_invalid_signing_params_throws_exception_with_correct_msg(arg, exception
     assert exception_msg in error.value.args
 
 
-def test_parse_signing_option_cms_with_engine():
+def test_parse_signing_option_cms_with_engine_and_keyform():
     # Only engine
     obj = main.parse_signing_option("CMS,foo,bar,baz,qux", engine="myengine")
     assert isinstance(obj, SWUSignCMS)
@@ -164,11 +164,30 @@ def test_parse_signing_option_cms_with_engine():
     assert obj.passin == "baz"
     assert obj.certfile == "qux"
 
+    # Only keyform
+    obj = main.parse_signing_option("CMS,foo,bar,baz,qux", keyform="mykeyform")
+    assert isinstance(obj, SWUSignCMS)
+    assert obj.keyform == "mykeyform"
+    assert obj.key == "foo"
+    assert obj.cert == "bar"
+    assert obj.passin == "baz"
+    assert obj.certfile == "qux"
 
-    # Without engine
+    # Both engine and keyform
+    obj = main.parse_signing_option("CMS,foo,bar,baz,qux", engine="myengine", keyform="mykeyform")
+    assert isinstance(obj, SWUSignCMS)
+    assert obj.engine == "myengine"
+    assert obj.keyform == "mykeyform"
+    assert obj.key == "foo"
+    assert obj.cert == "bar"
+    assert obj.passin == "baz"
+    assert obj.certfile == "qux"
+
+    # Neither engine nor keyform
     obj = main.parse_signing_option("CMS,foo,bar,baz,qux")
     assert isinstance(obj, SWUSignCMS)
     assert obj.engine is None
+    assert obj.keyform is None
     assert obj.key == "foo"
     assert obj.cert == "bar"
     assert obj.passin == "baz"
