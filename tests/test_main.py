c2137e524e313781fa06bf152e0f09ee115a98c6
Javier Fernandez Pastrana <javier.pastrana@linutronix.de>
2025-05-23 12:59:40 +0200

    Add test cases for CMS engine option
    Add two new test cases: one with the engine option enabled and one
    without.
    
    Signed-off-by: Javier Fernandez Pastrana <javier.pastrana@linutronix.de>

--- tests/test_main.py
+++ tests/test_main.py
@@ -154,6 +154,27 @@ def test_invalid_signing_params_throws_exception_with_correct_msg(arg, exception
     assert exception_msg in error.value.args
 
 
+def test_parse_signing_option_cms_with_engine():
+    # Only engine
+    obj = main.parse_signing_option("CMS,foo,bar,baz,qux", engine="myengine")
+    assert isinstance(obj, SWUSignCMS)
+    assert obj.engine == "myengine"
+    assert obj.key == "foo"
+    assert obj.cert == "bar"
+    assert obj.passin == "baz"
+    assert obj.certfile == "qux"
+
+
+    # Without engine
+    obj = main.parse_signing_option("CMS,foo,bar,baz,qux")
+    assert isinstance(obj, SWUSignCMS)
+    assert obj.engine is None
+    assert obj.key == "foo"
+    assert obj.cert == "bar"
+    assert obj.passin == "baz"
+    assert obj.certfile == "qux"
+
+
 #### Parse args tests ####
 @pytest.fixture
 def mock_main_funcs(monkeypatch):
